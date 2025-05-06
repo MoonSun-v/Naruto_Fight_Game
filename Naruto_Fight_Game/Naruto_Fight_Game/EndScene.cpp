b85bb22bcee1a99a047d46b1069e2c2e2cc45a2f
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2025-05-07 05:43:30 +0900

    [Design] End Scene 구성

--- Naruto_Fight_Game/Naruto_Fight_Game/EndScene.cpp
+++ Naruto_Fight_Game/Naruto_Fight_Game/EndScene.cpp
@@ -2,12 +2,17 @@
 #include "EndScene.h"
 #include "MainApp.h"
 
+#include "Background.h"
+
 #include "../GDIEngine_StaticLib/SceneManager.h"
 #include "../GDIEngine_StaticLib/InputManager.h"
 
 void EndScene::Enter()
 {
 	__super::Enter();
+
+	// 硅版 按眉 积己 
+	Background* pBackground = CreateObject<Background>(L"../Resources/EndScene.png");
 }
 
 void EndScene::Exit()
@@ -18,16 +23,13 @@ void EndScene::Exit()
 void EndScene::Update()
 {
 	__super::Update();
-
-	if (InputManager::Get().IsKeyPressed(VK_SPACE)) // 单捞磐 秦力 贸府 镭瘤 富鲤! 
-	{
-		SceneManager::Get().ChangeScene(MainApp::SceneType::Scene_Start);
-	}
+	 
+	if (InputManager::Get().IsKeyPressed(VK_SPACE))  SceneManager::Get().ChangeScene(MainApp::SceneType::Scene_Start);
 }
 
 void EndScene::Render()
 {
 	__super::Render();
 
-	RenderManager::Get().DrawText_w(L"EndScene", 10, 10, 30, Gdiplus::Color::Yellow);
+	// RenderManager::Get().DrawText_w(L"EndScene", 10, 10, 30, Gdiplus::Color::Yellow);
 }
