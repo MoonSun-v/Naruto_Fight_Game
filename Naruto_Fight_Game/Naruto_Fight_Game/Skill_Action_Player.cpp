11c8af8aa19e95bcaab29ea25663407122ff92c8
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2025-05-06 22:11:38 +0900

    [Feat] Skill 충돌 구현

--- Naruto_Fight_Game/Naruto_Fight_Game/Skill_Action_Player.cpp
+++ Naruto_Fight_Game/Naruto_Fight_Game/Skill_Action_Player.cpp
@@ -2,6 +2,8 @@
 #include "Player.h"
 #include "Skill_Action_Player.h"
 #include "Idle_Action_Player.h"
+#include "SceneManager.h"
+#include "Scene.h"
 
 void Skill_Action_Player::Enter(Player* player)
 {
@@ -10,6 +12,34 @@ void Skill_Action_Player::Enter(Player* player)
 
 void Skill_Action_Player::Update(Player* player, float deltaTime)
 {
+    elapsed += deltaTime;
+
+    if (m_MoveDistance < m_TotalDistance && elapsed >= 5.0f)
+    {
+        float moveStep = m_MoveSpeed * deltaTime;
+        m_MoveDistance += moveStep;
+
+        Vector2 dir = player->IsFlipX() ? Vector2(-1, 0) : Vector2(1, 0);
+        player->SetPosition(player->GetPosition() + dir * moveStep);
+
+        if (!m_DamageApplied)
+        {
+            for (auto other : SceneManager::Get().GetCurrentScene()->GetObjects())
+            {
+                Player* enemy = dynamic_cast<Player*>(other);
+                if (enemy && enemy != player)
+                {
+                    if (player->GetAABB().CheckIntersect(enemy->GetAABB()))
+                    {
+                        enemy->TakeDamage(20.0f);
+                        m_DamageApplied = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
     if (player->GetAnimator().IsAnimationFinished())
     {
         player->ChangeActionState(new Idle_Action_Player());
--- Naruto_Fight_Game/Naruto_Fight_Game/Skill_Action_Player.h
+++ Naruto_Fight_Game/Naruto_Fight_Game/Skill_Action_Player.h
@@ -7,4 +7,11 @@ public:
     void Enter(Player* player) override;
     void Update(Player* player, float deltaTime) override;
     void Exit(Player* player) override;
+
+    float elapsed = 0.0f;
+
+    float m_MoveSpeed = 300.0f; 
+    float m_TotalDistance = 600.0f;
+    float m_MoveDistance = 0.0f;
+    bool m_DamageApplied = false;
 };
